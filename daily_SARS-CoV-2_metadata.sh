a79a6e2bcf2825b0d344b544daaff78dba52ec98
Erin Young <erin.olde@gmail.com>
2022-08-17 16:11:51 -0600

    updated paths

--- daily_SARS-CoV-2_metadata.sh
+++ daily_SARS-CoV-2_metadata.sh
@@ -11,48 +11,54 @@ Version: 2022-02-28
 
 echo "$USAGE"
 
-while [ -d "/Volumes/IDGenomics_NAS/NextStrain" ]
+while [ -d "/Volumes/IDGenomics_NAS/" ]
 do
   current_date=$(date +"%Y-%m-%d")
   echo "$(date) : the current date is $current_date"
 
-  if [ ! -d "/Volumes/IDGenomics_NAS/NextStrain/$current_date" ]
+  if [ ! -d "/Volumes/IDGenomics_NAS/COVID/daily_metadata/$current_date" ]
   then
-
-    # copy over jsons that haven't been copied over : ls /Volumes/IDGenomics_NAS/NextStrain/jsons/UT*json | grep -v "missing" | rev | cut -f 1 -d "/" | rev | cut -f 1 -d "." | parallel ls /Volumes/IDGenomics_NAS/NextStrain/working_json/{}.json
-    mkdir -p /Volumes/IDGenomics_NAS/NextStrain/$current_date/logs
-    err_file=/Volumes/IDGenomics_NAS/NextStrain/$current_date/logs/err.txt
-    log_file=/Volumes/IDGenomics_NAS/NextStrain/$current_date/logs/log.txt
+    mkdir -p /Volumes/IDGenomics_NAS/COVID/daily_metadata/$current_date/logs
+    err_file=/Volumes/IDGenomics_NAS/COVID/daily_metadata/$current_date/logs/err.txt
+    log_file=/Volumes/IDGenomics_NAS/COVID/daily_metadata/$current_date/logs/log.txt
     echo "$(date) : It is a new day! Time to get some metadata together" 2>> $err_file | tee -a $log_file
-    echo "$(date) : metadata will be in /Volumes/IDGenomics_NAS/NextStrain/$current_date" 2>> $err_file | tee -a $log_file
+    echo "$(date) : metadata will be in /Volumes/IDGenomics_NAS/COVID/daily_metadata/$current_date/dripping_rock" 2>> $err_file | tee -a $log_file
 
-    ls /Volumes/NGS/Analysis/covidseq/UT* -d | rev | cut -f 1 -d "/" | rev | parallel ls /Volumes/IDGenomics_NAS/NextStrain/jsons/{}.json 2>&1 | grep "No such" | awk '{print $4}' | cut -d "/" -f 6  | cut -f 1 -d "." | parallel /home/Bioinformatics/Dripping_Rock/metadata/fasta_to_json.py {}
-    jsons=($(ls /Volumes/IDGenomics_NAS/NextStrain/jsons/UT*.json | grep -v "missing.json" | rev | cut -f 1 -d "/" | rev))
+    # comparing runs to json files
+    ls /Volumes/NGS/Analysis/covidseq/UT* -d | rev | cut -f 1 -d "/" | rev | \
+      parallel ls /Volumes/IDGenomics_NAS/COVID/daily_metadata/json/{}.json 2>&1 | \
+      grep "No such" | awk '{print $4}' | cut -d "/" -f 6  | cut -f 1 -d "." | \
+      parallel python /home/Bioinformatics/Dripping_Rock/bin/fasta_to_json.py {}
+
+    # comparing automatically created json files with working json files
+    jsons=($(ls /Volumes/IDGenomics_NAS/COVID/daily_metadata/json/UT*.json | grep -v "missing.json" | rev | cut -f 1 -d "/" | rev))
     for json in ${jsons[@]}
     do
-      if [ ! -f "/Volumes/IDGenomics_NAS/NextStrain/working_json/$json" ]
+      if [ ! -f "/Volumes/IDGenomics_NAS/COVID/daily_metadata/working_json/$json" ]
       then
-        cp /Volumes/IDGenomics_NAS/NextStrain/jsons/$json /Volumes/IDGenomics_NAS/NextStrain/working_json/.
+        cp /Volumes/IDGenomics_NAS/COVID/daily_metadata/json/$json /Volumes/IDGenomics_NAS/COVID/daily_metadata/working_json/.
       fi
     done
 
-    cd /Volumes/IDGenomics_NAS/NextStrain/$current_date
+    # starting Dripping Rock
+    cd /Volumes/IDGenomics_NAS/COVID/daily_metadata/$current_date
     echo "$(date) : Running Dripping_Rock to get the metadata together" 2>> $err_file | tee -a $log_file
-    nextflow /home/Bioinformatics/Dripping_Rock/metadata/nextstrain_metadata.nf -with-tower -dsl1 2>> $err_file | tee -a $log_file
-    echo "$(date) : The metadata file should be at /Volumes/IDGenomics_NAS/NextStrain/$current_date/Dripping_Rock/UPHL_metadata.csv" 2>> $err_file | tee -a $log_file
+    nextflow run /home/Bioinformatics/Dripping_Rock -with-tower 2>> $err_file | tee -a $log_file
+    echo "$(date) : The metadata file should be at /Volumes/IDGenomics_NAS/COVID/daily_metadata/$current_date/dripping_rock/UPHL_metadata.csv" 2>> $err_file | tee -a $log_file
 
     if [ "$(date +%u)" -eq "7" ]
     then
       echo "$(date) : It's Sunday, time to get a new tree" 2>> $err_file | tee -a $log_file
-      nextflow /home/Bioinformatics/Dripping_Rock/metadata/nextstrain_metadata.nf -with-tower -resume --msa true -dsl1
+      nextflow run /home/Bioinformatics/Dripping_Rock -with-tower -resume --msa true
     fi
 
   fi
 
+  # print a slight QC output that compares runs to the jsons
   covidseq_runs=($(ls /Volumes/NGS/Analysis/covidseq/UT-* -d | grep -v " " | rev | cut -f 1 -d "/" | rev))
   for run in ${covidseq_runs[@]}
   do
-    if [ ! -f "/Volumes/IDGenomics_NAS/NextStrain/working_json/$run.json" ]
+    if [ ! -f "/Volumes/IDGenomics_NAS/COVID/daily_metadata/working_json/$run.json" ]
     then
       echo "$(date) : $run cannot be included in the metadata file"
     fi
