a79cb8e78d42db3572b5fc7ea456fa436337b836
John Arnn <72882088+jwarnn@users.noreply.github.com>
2023-06-06 14:22:21 -0600

    Update and rename auto_Grandeur_ICA.py to auto_grandeur_ICA.py

--- auto_grandeur_ICA.py
+++ auto_grandeur_ICA.py
@@ -38,7 +38,7 @@ now = datetime.datetime.now()
 print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
 
-bashCommand = "icav2 projects enter Testing"
+bashCommand = "icav2 projects enter Produciton"
 process = subprocess.Popen(bashCommand.split(" ",3), stdout=subprocess.PIPE)
 process.communicate()
 
@@ -48,22 +48,24 @@ if len(sys.argv) > 2:
 else:
     additional_ref=''
 
-bashCommand = "icav2 projectdata upload /Volumes/IDGenomics_NAS/WGS_Serotyping/%s/Sequencing_reads/Raw /%s/" % (run_name,run_name)
-process = subprocess.Popen(bashCommand.split(" ",3), stdout=subprocess.PIPE)
-process.communicate()
 
-time.sleep(300)
-
-bashCommand = "icav2 projectdata list --parent-folder /%s/ --match-mode FUZZY" % run_name
+bashCommand = "icav2 projectdata list --file-name %s --match-mode FUZZY --parent-folder /" % (run_name[-6:]+"_"+run_name[3:9])
 tmp= icav_out(bashCommand)
-ica_id=tmp[0].split()[4]
+ica_folder=tmp[0].split()[0]
+
+bashCommand = "icav2 projectdata list  --match-mode FUZZY --parent-folder /%s/Alignment_1/" % ica_folder
+tmp= icav_out(bashCommand)
+ica_folder2=tmp[0].split()[0]
+
+bashCommand = "icav2 projectdata list  --match-mode FUZZY --parent-folder /%s/Alignment_1/%s/ --file-name Fastq" % (ica_folder,ica_folder2)
+tmp= icav_out(bashCommand)
+ica_id=tmp[0].split()[3]
 
 # Create icav2 argument
-arg = "icav2 projectpipelines start nextflow UPHL-BioNGS_Granduer_VGrandeur-3_0_20230310_0 --input reads:%s " % ica_id
-arg = arg + "--input project_dirs:fol.fb0091ef9e4d4e515b6408da63045a75,fol.90b6b7865bc7434384e408da6377c840,fol.8802646a4148485884df08da6377c840,fol.17212ac619d44b68296008da58d44868,fol.756293fdaa3942cc3b9f08da58d44869,fol.97c3321c555a4b1c672f08da58d44868 "
-arg = arg + "--user-reference %s_Granduer_Analysis%s" % (run_name,additional_ref)
+arg = "icav2 projectpipelines start nextflow UPHL-BioNGS_Granduer_V3_1_20230523 --input reads:%s " % ica_id
+arg = arg + "--input project_dirs:fol.8d6ce115bc654a98b4b908db5cb0c8b4,fol.4cfbf2608f0542e8822a08db520d1521,fol.083bc5fdf9cc4a1fb4d708db5cb0c8b4,fol.d8e135fc30984285829c08db520d1521,fol.258c3c406d844f39822208db520d1521,fol.17e932b9ea87429b824e08db520d1521,fol.3215c978710246b6694308daf8fa4ab4,fol.34b4cd1b57ab4c2331fd08db45a02d4f,fol.dc7067b8edf9407943ef08db4a610552 "
+arg = arg + "--user-reference %s_Granduer_Analysis%s --storage-size medium" % (run_name,additional_ref)
 
 print(arg)
 
-process = subprocess.Popen(arg.split(" "), stdout=subprocess.PIPE)
 os.system(arg)
