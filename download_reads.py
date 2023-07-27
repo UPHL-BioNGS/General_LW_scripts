8f3023345fd775c1b6d6414b700c6d4799d6a9b3
John Arnn <72882088+jwarnn@users.noreply.github.com>
2023-07-27 09:03:06 -0600

    Update download_reads.py
    added logging

--- download_reads.py
+++ download_reads.py
@@ -7,14 +7,46 @@ import re
 import time
 import pandas as pd
 import numpy as np
-from datetime import date
+from datetime import date,datetime
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from importlib.machinery import SourceFileLoader
+from typing import Union
+import functools
+import logging
+
+logging.basicConfig(filename='/Volumes/IDGenomics_NAS/Bioinformatics/jarnn/analysis_for_run.log', format='%(asctime)s - %(name)s - %(message)s')
+
+# create logger
+logger = logging.getLogger('download_reads.py')
+logger.setLevel(logging.DEBUG)
+
+def log(_func=None, *, my_logger):
+    def decorator_log(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            logger = my_logger
+            args_repr = [repr(a) for a in args]
+            kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+            signature = ", ".join(args_repr + kwargs_repr)
+            logger.debug(f"function {func.__name__} called with args {signature}")
+            try:
+                result = func(*args, **kwargs)
+                return result
+            except Exception as e:
+                logger.exception(f"Exception raised in {func.__name__}. exception: {str(e)}")
+                raise e
+        return wrapper
+
+    if _func is None:
+        return decorator_log
+    else:
+        return decorator_log(_func)
 
 run_name = str(sys.argv[1])
 run_type = str(sys.argv[2])
 
+@log(my_logger=logger)
 def bs_out(bashCommand):
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
@@ -35,6 +67,8 @@ config = SourceFileLoader("config","/Volumes/IDGenomics_NAS/Bioinformatics/jarnn
 
 client = WebClient(token=config.token)
 channel_id = config.channel_id
+
+@log(my_logger=logger)
 def slack_message(string):
     try:
 
@@ -44,12 +78,12 @@ def slack_message(string):
         )
 
     except:
-        print("Slack Error")
+        logger.info("Slack Error")
 
 
 # Make dir that reads and anaylsis files will be saved to
 
-if run_type == "granduer":
+if run_type == "grandeur":
     path = "/Volumes/IDGenomics_NAS/pulsenet_and_arln/%s/reads"% run_name
 if run_type == "mycosnp":
     path = "/Volumes/IDGenomics_NAS/fungal/%s/Sequencing_reads/Raw"% run_name
@@ -57,7 +91,7 @@ if run_type == "mycosnp":
 try:
     os.makedirs(path)
 except:
-    print("Directory Already Exits, Make sure Reads are not already downloaded and delete Directory %s" % datetime.now())
+    logger.info("Directory Already Exits, Make sure Reads are not already downloaded and delete Directory %s" % datetime.now())
     sys.exit()
 
 
@@ -100,5 +134,5 @@ for i in idds:
     """ % (i,path)
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-print("Reads %s at %s" % (path,datetime.now()))
+logger.info("Reads %s at %s" % (path,datetime.now()))
 slack_message("Reads downloaded into %s" % path)
