ec86726cba91c6790c11932c952d3e6eb3fa4236
eriny <erin.olde@gmail.com>
2024-07-08 15:13:38 -0600

    added sample sheet

--- download_reads.py
+++ download_reads.py
@@ -15,6 +15,9 @@ from typing import Union
 import functools
 import logging
 
+# local functions
+from aws_samplesheet_grandeur_create import grandeur_sample_sheet
+
 logging.basicConfig(filename='/Volumes/IDGenomics_NAS/Bioinformatics/jarnn/analysis_for_run.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # create logger
@@ -43,9 +46,6 @@ def log(_func=None, *, my_logger):
     else:
         return decorator_log(_func)
 
-run_name = str(sys.argv[1])
-run_type = str(sys.argv[2])
-
 @log(my_logger=logger)
 def bs_out(bashCommand):
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
@@ -58,81 +58,99 @@ def bs_out(bashCommand):
     tmp.pop(-1)
     tmp=[x.replace('|', '') for x in tmp]
     tmp=[re.sub(r"\s+", ' ', x) for x in tmp]
+    logging.info("tmp value %s" % (tmp))
     return tmp
+
+# download the sample sheet used to generate reads from basespace
+def download_sample_sheet(path, idd):
+    bashCommand = "bs run download --config=bioinfo --output=%s --id=%s --extension=csv" % (path,idd)
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    logging.info("Sample sheet downloaded to %s" % (path))
+
+def download_fastq_files(path, idd):
+    idds = get_sample_ids(idd)
+
+    # Download Reads from Run
+    for i in idds:
+        bashCommand ="bs download dataset --config=bioinfo --id=%s --output=%s/ --retry" % (i,path)
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+    
+    logging.info("Reads downloaded to %s" % (path))
+
+def get_sample_ids(idd):
+    # Now the BSSH ids must be collected into a list
+    bashCommand = "bs list dataset --config=bioinfo --input-run=%s " % (idd)
+    tmp=bs_out(bashCommand)
+    for i in range(len(tmp)):
+        if tmp[i].split()[0]== 'Undetermined':
+            del tmp[i]
+            del tmp[i]
+            break
+    tmp=pd.DataFrame(columns=tmp[0].split()[1:], data=[row.split()[1:4] for row in tmp[1:]])
+    idds=list(tmp['Id'])
+    logging.info("Reads ids are %s" % (idds))
+    return idds
+
+# Get the basespace run id from run name
+def get_run_id(run_name):
+    # Once the run is completed it will be downloaded
+    bashCommand='bs list --config=bioinfo runs'
+    tmp=bs_out(bashCommand)
+    tmp=pd.DataFrame(index=[row.split()[2] for row in tmp[1:]], columns=tmp[0].split()[1:], data=[row.split()[1:4] for row in tmp[1:]])
+    idd=tmp.at[run_name,'Id']
+    logging.info("Run id is %s" % (idd))
+    return idd
 
 # Slack_sdk values needed for sending messages to Slack. Uses an API already set up on the Slack website for the UPHL Workspace to post messages on the notifications channel.
 # Function makes sending Slack messages as easy as using the print funcition.
-
-config = SourceFileLoader("config","/Volumes/IDGenomics_NAS/Bioinformatics/jarnn/config.py").load_module()
-
-client = WebClient(token=config.token)
-channel_id = config.channel_id
-
 @log(my_logger=logger)
 def slack_message(string):
-    try:
+    config = SourceFileLoader("config","/Volumes/IDGenomics_NAS/Bioinformatics/jarnn/config.py").load_module()
 
+    client = WebClient(token=config.token)
+    channel_id = config.channel_id
+
+    try:
         result = client.chat_postMessage(
             channel=channel_id,
             text=string
         )
-
     except:
-        logger.info("Slack Error")
+        logger.fatal("Slack Error")
 
+def main():
+    if len(sys.argv) != 3:
+        logging.fatal("Required positional args not found:")
+        logging.info("Usage: %s run_name run_type" % sys.argv[0])
+        sys.exit(0)
 
-# Make dir that reads and anaylsis files will be saved to
+    run_name = str(sys.argv[1])
+    run_type = str(sys.argv[2])
 
-if run_type == "grandeur":
-    path = "/Volumes/IDGenomics_NAS/pulsenet_and_arln/%s/reads"% run_name
-if run_type == "mycosnp":
-    path = "/Volumes/IDGenomics_NAS/fungal/%s/Sequencing_reads/Raw"% run_name
+    # Make dir that reads and anaylsis files will be saved to
+    if run_type == "grandeur":
+        path = "/Volumes/IDGenomics_NAS/pulsenet_and_arln/%s/reads"% run_name
+    elif run_type == "mycosnp":
+        path = "/Volumes/IDGenomics_NAS/fungal/%s/Sequencing_reads/Raw"% run_name
 
-try:
-    os.makedirs(path)
-except:
-    logger.info("Directory Already Exits, Make sure Reads are not already downloaded and delete Directory %s" % datetime.now())
-    sys.exit()
+    try:
+        os.makedirs(path)
+    except:
+        logger.fatal("Directory Already Exits, Make sure Reads are not already downloaded and delete Directory %s" % datetime.now())
+        sys.exit()
 
+    idd = get_run_id(run_name)
 
-# Once the run is completed it will be downloaded
-bashCommand='bs list --config=bioinfo runs'
-tmp=bs_out(bashCommand)
-tmp=pd.DataFrame(index=[row.split()[2] for row in tmp[1:]], columns=tmp[0].split()[1:], data=[row.split()[1:4] for row in tmp[1:]])
-idd=tmp.at[run_name,'Id']
+    download_sample_sheet(path, idd)
 
+    download_fastq_files(path, idd)
 
-# First the samplesheet must be downloaded
-bashCommand = """bs run download --config=bioinfo
-          --output="%s"
-          --id=%s
-          --extension=csv
-""" % (path,idd)
+    grandeur_sample_sheet(run_name, path + "/SampleSheet.csv", path)
 
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
+    logger.info("Reads and sample sheet downloaded to %s at %s" % (path,datetime.now()))
+    slack_message("Reads downloaded to %s" % path)
 
-# Now the BSSH ids must be collected into a list
-bashCommand = "bs list dataset --config=bioinfo --input-run=%s " % (idd)
-tmp=bs_out(bashCommand)
-for i in range(len(tmp)):
-    if tmp[i].split()[0]== 'Undetermined':
-        del tmp[i]
-        del tmp[i]
-        break
-tmp=pd.DataFrame(columns=tmp[0].split()[1:], data=[row.split()[1:4] for row in tmp[1:]])
-idds=list(tmp['Id'])
-
-# Download Reads from Run
-for i in idds:
-    bashCommand ="""
-    bs download dataset \
-            --config=bioinfo \
-            --id=%s \
-            --output="%s/" \
-            --retry
-    """ % (i,path)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-logger.info("Reads %s at %s" % (path,datetime.now()))
-slack_message("Reads downloaded into %s" % path)
+if __name__ == "__main__":
+    main()
